3c4dd954ea64d5ce451a7c5f273fcfce33b7ed82
zz bct <1668856072@qq.com>
2017-11-21 20:12:10 +0800

    add router files

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'); //express模块
+var path = require('path');//路径模块
+var favicon = require('serve-favicon');//请求网页的logo
+var logger = require('morgan');//在控制台中，显示req请求的信息
+var cookieParser = require('cookie-parser');//解析Cookie的工具，通过req.cookie可以取到传过来的cookie,并把它们转成对象。
+var bodyParser = require('body-parser');//中间件，用于处理json,raw,text和url编码的数据
 
+//路由信息
 var index = require('./routes/index');
 var users = require('./routes/users');
+var create = require('./routes/create');
+var del = require('./routes/del');
+var edit = require('./routes/edit');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views')); //设置视图根目录
+app.set('view engine', 'jade');//设计视图模板引擎
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//载入中间件
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//配置路由
 app.use('/', index);
-app.use('/users', users);
+app.use('/users', users); //查
+app.use('/create', create); //增
+app.use('/del', del); //删
+app.use('/edit', edit); //改
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
