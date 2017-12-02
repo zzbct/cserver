ed47d8f2b7f5842510fc8c34f2fc6c7828d2b8b7
zz bct <1668856072@qq.com>
2017-12-02 17:55:54 +0800

    test sql connect

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var db = require('../db');
 var router = express.Router();
 
 db.connect();
-var  sql = 'SELECT * FROM user';
+var  sql = 'SELECT * FROM user'; //获取用户信息
+var goalsSql = '';
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,4 +18,36 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/goals',function (req,res) {
+  var sql = `SELECT CheckItem FROM reviewitem where Stage='${req.query.stage}' and Customize=${req.query.character}`
+  db.query(sql,function (err, result) {
+    if(err){
+      console.log('[SELECT ERROR] - ',err.message);
+      return;
+    }
+    //把搜索值输出
+    res.send(result);
+  });
+})
+
+router.get('/subgoals',function (req,res) {
+  var sql1 = `SELECT ID FROM reviewitem where CheckItem='${req.query.parent}'`
+  db.query(sql1,function (err, result) {
+    if(err || result.length == 0){
+      console.log('[SELECT ERROR] - ',err.message);
+      return;
+    }
+    //把搜索值输出
+    var refItem = result[0].ID;
+    var sql2 = `SELECT EviItem FROM eviitem where RefRItem=${refItem}`
+    db.query(sql2,function (err, result) {
+      if (err) {
+        console.log('[SELECT ERROR] - ', err.message);
+        return;
+      }
+      res.send(result)
+    })
+  });
+
+})
 module.exports = router;
