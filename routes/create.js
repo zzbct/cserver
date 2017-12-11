bf9eef4d942f177da4e40f74008912dcf6540bab
zz bct <1668856072@qq.com>
2017-12-11 15:15:09 +0800

    login for get id

--- routes/create.js
+++ routes/create.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var router = express.Router();
 var http = require('http');
+var router = express.Router();
 
 /* put users listing. */
 router.get('/', function(req,res){
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var http = require('http');
+var encoding = require('encoding');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET login page. */
+router.post('/', function(req,res){
+  var opt={
+    host:'192.168.109.140',
+    port:'8080',
+    method:'POST',
+    path:'http://192.168.109.140:8080/sunapp/login',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  };
+  var content = {
+    UserName: req.body.user,
+    Password: req.body.passward
+  }
+  var request = http.request(opt, function(resq) {
+    resq.on('data',function(data){
+      res.send(data);
+    })
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  })
+  request.write(JSON.stringify(content));
+  request.end();
 });
 
 module.exports = router;
